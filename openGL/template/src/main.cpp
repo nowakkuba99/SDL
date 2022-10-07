ab65900e93287bc68abfe71eab49f1afd9b9420a
Jakub Nowak <nowakkuba99@gmail.com>
2022-10-07 15:00:08 +0200

    Rectangle drawing

--- openGL/template/src/main.cpp
+++ openGL/template/src/main.cpp
@@ -1,5 +1,5 @@
+#define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-
 int main(void)
 {
     GLFWwindow* window;
@@ -9,7 +9,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Sorting algorithms comparasion", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -18,20 +18,21 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    GLdouble x1 = -1, y1 = -1, x2 = 1, y2 = 1;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
+        glColor3b(100,100,100);
+        glRectd(x1,y1,x2,y2);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    
     glfwTerminate();
     return 0;
 }
