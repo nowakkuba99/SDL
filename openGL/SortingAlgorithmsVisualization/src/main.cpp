d1098dcb0f784cb3aa8762448b3f1d2dfdce5be2
Jakub Nowak <nowakkuba99@gmail.com>
2022-10-21 19:34:12 +0200

    Main function update

--- openGL/SortingAlgorithmsVisualization/src/main.cpp
+++ openGL/SortingAlgorithmsVisualization/src/main.cpp
@@ -2,11 +2,19 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
-
+#include <string>
 
 #include "drawFun.hpp"
 #include "arrayFunctions.hpp"
 #include "sortingFun.hpp"
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main(void)
 {
@@ -18,7 +26,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Sorting Algorithms Visualizer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -27,22 +35,53 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    glfwSetKeyCallback(window, key_callback);
     std::vector<std::shared_ptr<draw::Rectangle>> RectVec;
-    array::initArr(RectVec);
     bool finished = false;
-    int comps, swaps;
+    int comps = 0, swaps = 0;
+    std::string type = "bubblesort";
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         if(!finished)
         {
-            //finished = sort::mainSortFun(window, &sort::bubbleSort,RectVec,swaps,comps);
-            finished = sort::mainSortFun(window, &sort::quickSort,RectVec, swaps, comps);
-            std::cout<<"----------------------------------------"<<"\n";
-            std::cout<<"Finished!"<<'\n';
-            std::cout<<"Num of Comp: "<<comps<<"\n";
-            std::cout<<"Num of Swaps: "<<swaps<<"\n";
+            if(type == "bubblesort")
+            {
+                /* Bubble sort */
+                array::initArr(RectVec);
+                comps = 0; swaps = 0;
+                finished = sort::mainSortFun(window, &sort::bubbleSort,RectVec,swaps,comps);
+                std::cout<<"----------------------------------------"<<"\n";
+                std::cout<<"Finished Bubble Sort!"<<'\n';
+                std::cout<<"Num of Comp: "<<comps<<"\n";
+                std::cout<<"Num of Swaps: "<<swaps<<"\n";
+
+                finished = false;
+                type = "selectionsort";
+            }
+            else if (type == "quicksort")
+            {
+                /* Quick sort */
+                array::initArr(RectVec);
+                comps = 0; swaps = 0;
+                finished = sort::mainSortFun(window, &sort::quickSort,RectVec, swaps, comps);
+                std::cout<<"----------------------------------------"<<"\n";
+                std::cout<<"Finished Quick Sort!"<<'\n';
+                std::cout<<"Num of Comp: "<<comps<<"\n";
+                std::cout<<"Num of Swaps: "<<swaps<<"\n";
+            }
+            else if (type == "selectionsort")
+            {
+                /* Selection sort */
+                array::initArr(RectVec);
+                comps = 0; swaps = 0;
+                finished = sort::mainSortFun(window, &sort::selectionSort,RectVec, swaps, comps);
+                std::cout<<"----------------------------------------"<<"\n";
+                std::cout<<"Finished Selection Sort!"<<'\n';
+                std::cout<<"Num of Comp: "<<comps<<"\n";
+                std::cout<<"Num of Swaps: "<<swaps<<"\n";
+            }
+            
         }
         else
         {
