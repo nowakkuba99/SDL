cd11bf8a52436de4de83eded0210311f8befbd38
Jakub Nowak <nowakkuba99@gmail.com>
2022-12-24 13:35:50 +0100

    Added pre-compiled headers support

---
+++ openGL/RenderEngine/src/pch/pchBarrel.hpp
@@ -0,0 +1,18 @@
+#ifndef E0B05701_1CE6_4C6D_A363_FE30540EBD41
+#define E0B05701_1CE6_4C6D_A363_FE30540EBD41
+
+/* Functional */
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+/* Data types */
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#endif /* E0B05701_1CE6_4C6D_A363_FE30540EBD41 */
