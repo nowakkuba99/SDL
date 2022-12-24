661475cd4cf9267dc4954b0cf5f96c012d8219a1
Jakub Nowak <nowakkuba99@gmail.com>
2022-12-24 13:23:19 +0100

    Upadted logging to support custom data types

--- openGL/RenderEngine/src/barrel/Log.hpp
+++ openGL/RenderEngine/src/barrel/Log.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/ostr.h"
 // User defined includes
 
 namespace Barrel
@@ -26,5 +27,19 @@ namespace Barrel
     };
     
 }
+
+/* Core Macros defines */
+#define BR_CORE_TRACE(...)  ::Barrel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define BR_CORE_INFO(...)   ::Barrel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define BR_CORE_WARN(...)   ::Barrel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define BR_CORE_ERROR(...)  ::Barrel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define BR_CORE_FATAL(...)  ::Barrel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+/* Client Macros defines */
+#define BR_TRACE(...)  ::Barrel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define BR_INFO(...)   ::Barrel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define BR_WARN(...)   ::Barrel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define BR_ERROR(...)  ::Barrel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define BR_FATAL(...)  ::Barrel::Log::GetClientLogger()->fatal(__VA_ARGS)
 
 #endif /* B48B6F7C_C8ED_4F71_AB61_6735D6F9879E */
